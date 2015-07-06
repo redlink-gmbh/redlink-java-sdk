e0c82dfbd50ae5689f9cc60cf1a64b8ddbcff63e
Sergio Fernández <sergio@wikier.org>
2015-07-06 13:19:05 +0200

    reformatted

--- src/test/java/io/redlink/sdk/GenericTest.java
+++ src/test/java/io/redlink/sdk/GenericTest.java
@@ -2,9 +2,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,27 +26,27 @@ import java.io.InputStreamReader;
 
 /**
  * Generic tests and utilities shared by other tests in the suite
- * 
+ *
  * @author sergio.fernandez@redlink.co
  *
  */
 public class GenericTest {
 
     private static Logger log = LoggerFactory.getLogger(GenericTest.class);
-	
-	protected static final String API_KEY_FILE = "/api.key";
-	
-	/**
-	 * Build the credentials for testing the SDK. It dynamically
-	 * check for a file 'api.key' in the same path where the test
-	 * is, creating a DefaultCredentials with the API key,
-	 * CustomCredentials against the demo otherwise.
-	 * 
-	 * @return credentials
-	 */
-	protected static final Credentials buildCredentials() {
-		return buildCredentials(GenericTest.class);
-	}
+
+    protected static final String API_KEY_FILE = "/api.key";
+
+    /**
+     * Build the credentials for testing the SDK. It dynamically
+     * check for a file 'api.key' in the same path where the test
+     * is, creating a DefaultCredentials with the API key,
+     * CustomCredentials against the demo otherwise.
+     *
+     * @return credentials
+     */
+    protected static final Credentials buildCredentials() {
+        return buildCredentials(GenericTest.class);
+    }
 
     protected static final String buildDatasetsBaseUri(Credentials credentials, int user) {
         return UriBuilder.fromUri(credentials.getDataHub()).path(String.valueOf(user)).build().toString() + "/";
@@ -55,34 +55,34 @@ public class GenericTest {
     protected static final String buildDatasetBaseUri(Credentials credentials, int user, String dataset) {
         return UriBuilder.fromUri(credentials.getDataHub()).path(String.valueOf(user)).path(dataset).build().toString() + "/";
     }
-	
-	/**
-	 * Build the credentials for testing the SDK. It dynamically
-	 * check for a file 'api.key' in the same path where the test
-	 * is, creating a DefaultCredentials with the API key,
-	 * CustomCredentials against the demo otherwise.
-	 * 
-	 * @param klass Class to use for classpath-based file lookup
-	 * 
-	 * @return credentials
-	 */
-	protected static final Credentials buildCredentials(Class<?> klass) {
-		InputStream is = klass.getResourceAsStream(API_KEY_FILE);
-		if (is != null) {
-		    try {
-				InputStreamReader isr = new InputStreamReader(is);
-			    BufferedReader br = new BufferedReader(isr);
-				String apiKey = br.readLine();
-				return new DefaultCredentials(apiKey, getVersion());
-			} catch (IOException e) {
+
+    /**
+     * Build the credentials for testing the SDK. It dynamically
+     * check for a file 'api.key' in the same path where the test
+     * is, creating a DefaultCredentials with the API key,
+     * CustomCredentials against the demo otherwise.
+     *
+     * @param klass Class to use for classpath-based file lookup
+     *
+     * @return credentials
+     */
+    protected static final Credentials buildCredentials(Class<?> klass) {
+        InputStream is = klass.getResourceAsStream(API_KEY_FILE);
+        if (is != null) {
+            try {
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String apiKey = br.readLine();
+                return new DefaultCredentials(apiKey, getVersion());
+            } catch (IOException e) {
                 log.error("error reading api key file: {}", e.getMessage());
                 return null;
-			}
-		} else {
+            }
+        } else {
             log.error("api key file not found");
-			return null;
-		}
-	}
+            return null;
+        }
+    }
 
     private static String getVersion() {
         //FIXME: this does not work
