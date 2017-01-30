53a4a479b2bc8f2fe4f9007813da1d13270810fd
Sergio Fernández <sergio.fernandez@redlink.co>
2017-01-30 13:38:32 +0100

    prepared next dev iteration

--- src/test/java/io/redlink/sdk/util/VersionHelperTest.java
+++ src/test/java/io/redlink/sdk/util/VersionHelperTest.java
@@ -30,7 +30,7 @@ public class VersionHelperTest {
         System.out.println(new File("pom.xml").getAbsolutePath());
         final String version = VersionHelper.getVersion();
         Assert.assertNotNull(version);
-        Assert.assertEquals("1.0.3", version);
+        Assert.assertEquals("1.0.4-SNAPSHOT", version);
     }
 
     @Test
